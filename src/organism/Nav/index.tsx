864d15dc4d14b6bbc1baac59e4fbfe88bfdd14da
Denilson Rocha <eu.denilsonrocha@gmail.com>
2021-08-14 14:16:13 -0300

    add config prettier+eslint

---
+++ src/organism/Nav/index.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+
+const Nav = () => {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>List</li>
+        <li>Sobre</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Nav
